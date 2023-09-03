5cf61bc27f87a40cbdef95af69364ae4d2b56d78
LL-oyiso <theo.tose.tt@gmail.com>
2023-09-03 21:08:14 +0200

    nodemil code

---
+++ my-email/app.js
@@ -0,0 +1,11 @@
+// Create a transporter with your email service settings
+let transporter = nodemailer.createTransport(  {service: 'Gmail', // Use your email service provider
+auth: {
+  user: 'theo.tose.tt@gmail.com', // Your email address
+  pass: 'Gszr@9959', // Your email password
+},
+});
+
+
+// Rest of your code here
+
